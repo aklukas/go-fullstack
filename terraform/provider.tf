bbea9fa71693561424e8c84d434421db949641d6
aklukas <chris.take@venturplex.com>
2019-10-18 08:21:22 -0700

    feature(Terraform): create eks cluster

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,20 @@
 provider "aws" {
-  region     = var.AWS_REGION
+  version = ">= 2.28.1"
+  region  = var.AWS_REGION
+}
+
+provider "random" {
+  version = "~> 2.1"
+}
+
+provider "local" {
+  version = "~> 1.2"
+}
+
+provider "null" {
+  version = "~> 2.1"
+}
+
+provider "template" {
+  version = "~> 2.1"
 }
--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,3 +1,18 @@
 variable "AWS_REGION" {
   default = "us-east-1"
 }
+
+variable "CLUSTER_NAME" {
+  default = "ctake-eks-demo"
+  type    = "string"
+}
+
+variable "write_aws_auth_config" {
+  description = "Whether or not to output an auth K8s config-map file."
+  default = false
+}
+
+variable "write_kubeconfig" {
+  description = "Whether or not to output a K8s config file."
+  default = false
+}
